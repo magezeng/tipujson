cc1b0073d5ae67b2588cf690f99cf437f622d203
moke996 <694628923@qq.com>
2019-10-15 13:39:36 +0800

    Slice类型扫描，填值

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ type RunParamsUnit struct {
 }
 
 func main() {
-	fromString := `{"nick_name":"参数111111","name":"params1","type":1,"default":"haha","description":"该参数是在测试","index":1}`
-	var result RunParamsUnit
+	fromString := `[{"nick_name":"参数111111","name":"params1","type":1,"default":"haha","description":"该参数是在测试","index":1}]`
+	var result []RunParamsUnit
 
 	err := TipuJson.StringToObj(fromString, &result)
 	fmt.Println(err)
