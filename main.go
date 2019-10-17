190b302d5dad855f908806712ec4a73965b353e2
zengxianglin <429801517@qq.com>
2019-10-17 10:04:40 +0800

    StringToObject初版完全完成

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type RunParamsUnit struct {
-	Params      Params                              `json:"params"`
+	Params      *Params                             `json:"params"`
 	NickName    string                              `json:"nick_name"`   //在配置界面展示给用户的名称
 	Name        string                              `json:"name"`        //参数的名称，在策略内会以此名称进行调用
 	Type        RunParamsUnitType.RunParamsUnitType `json:"type"`        //参数的类型，类型分为   字符串  数字  布尔值  列表选择  四种
@@ -22,11 +22,11 @@ type Params struct {
 }
 
 func main() {
-	fromString := `{"params":{"nick_names":"哈哈","names":"子对象","types":2},"nick_name":"参数111111","name":"params1","type":1,"description":"该参数是在测试","index":1}`
+	fromString := `[{"params":{"nick_names":"哈哈","names":"子对象","types":2},"nick_name":"参数111111","name":"params1","type":1,"description":"该参数是在测试","index":1}]`
 	//result := []map[string]interface{}{}
-	//var result []map[string]interface{}
+	var result []map[string]interface{}
 	//result := new(RunParamsUnit)
-	var result **RunParamsUnit
+	//var result **RunParamsUnit
 	err := TipuJson.StringToObj(fromString, &result)
 	fmt.Println(err)
 	fmt.Println(result)
