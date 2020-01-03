ccd14b3b44174e8593c8c6e9dce96cdb2b76155e
zengxianglin <429801517@qq.com>
2020-01-03 11:00:48 +0800

    保留了BytesScanner定义，之后有可能将正则验证的方式重新修改为字节扫描的方式

---
+++ ObjectFillToToObject.go
@@ -0,0 +1,188 @@
+package tipujson
+
+import (
+	"fmt"
+	"github.com/magezeng/tipujson/ErrorMaker"
+	"reflect"
+)
+
+func ObjectFillToObject(fromObject interface{}, directionObject interface{}, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
+
+	directionType := reflect.TypeOf(directionObject)
+	directionValue := reflect.ValueOf(directionObject)
+	fromType := reflect.TypeOf(fromObject)
+	fromValue := reflect.ValueOf(fromObject)
+	return ObjectFillToObjectByReflect(fromType, fromValue, directionType, directionValue, sliceHandler)
+}
+
+func ObjectFillToObjectByReflect(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
+	return objectFillToObjectByReflectWithPositions(fromType, fromValue, waitSetType, waitSetValue, []string{}, sliceHandler)
+}
+
+func objectFillToObjectByReflectWithPositions(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, positions []string, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
+	if fromValue.IsZero() {
+		//遇到空值的情况直接不进行映射
+		return
+	}
+	//from必须是具体的值才进行下一轮   Interface和Ptr都代表from还未指向具体的值  所以不断遍历   直到from为值为止
+	for fromType.Kind() == reflect.Interface || fromType.Kind() == reflect.Ptr {
+		if fromType.Kind() == reflect.Interface {
+			fromType = fromValue.Elem().Type()
+			fromValue = fromValue.Elem()
+		} else {
+			fromType = fromType.Elem()
+			fromValue = fromValue.Elem()
+		}
+		if fromValue.IsZero() {
+			//遇到空值的情况直接不进行映射
+			return
+		}
+	}
+	switch waitSetType.Kind() {
+	case reflect.Ptr:
+		waitSetType = waitSetType.Elem()
+		if waitSetValue.IsNil() {
+			waitSetValue.Set(reflect.New(waitSetType))
+		}
+		waitSetValue = waitSetValue.Elem()
+		err = objectFillToObjectByReflectWithPositions(fromType, fromValue, waitSetType, waitSetValue, append(positions, waitSetType.Name()), sliceHandler)
+	case reflect.Struct:
+		err = ObjectFillToStruct(fromType, fromValue, waitSetType, waitSetValue)
+	case reflect.Slice:
+		//数组直接进行覆盖
+		waitSetValue.Set(fromValue)
+	case reflect.Interface:
+		//interface直接进行覆盖
+		waitSetValue.Set(fromValue)
+	case reflect.Map:
+		err = ObjectFillToMap(fromType, fromValue, waitSetType, waitSetValue)
+	default:
+		err = ObjectFillToBaseType(fromType, fromValue, waitSetType, waitSetValue)
+	}
+	return
+}
+
+func ObjectFillToStruct(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
+	if waitSetType.Kind() != reflect.Struct || (fromType.Kind() != reflect.Map && fromType.Kind() != reflect.Struct) {
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, waitSetType))
+		return
+	}
+	waitSetMap := map[string]struct {
+		waitSetType  reflect.Type
+		waitSetValue reflect.Value
+	}{}
+	for i := 0; i < waitSetType.NumField(); i++ {
+
+	}
+	for i := 0; i < waitSetType.NumField(); i++ {
+		valueField := waitSetValue.Field(i)
+		typeField := waitSetType.Field(i)
+		if typeField.Anonymous {
+			err = objectFillToObjectByReflectWithPositions(jsonObject, typeField.Type, valueField)
+			if err != nil {
+				return
+			}
+			continue
+		}
+		name := typeField.Name
+		jsonName := typeField.Tag.Get("json")
+		if jsonName == "" {
+			jsonName = name
+		}
+		subField, have := jsonObject.(map[string]interface{})[jsonName]
+		if !have {
+			continue
+		}
+		err = objectFillToObjectByReflectWithPositions(subField, typeField.Type, valueField)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func ObjectFillToMap(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
+	jsonObjectType := reflect.ValueOf(jsonObject).Kind()
+	// 假如说双方类型未匹配上，则直接出错退出
+	if jsonObjectType != reflect.Map || waitSetType.Kind() != reflect.Map {
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", jsonObjectType, waitSetType))
+		return
+	}
+	if waitSetValue.IsNil() {
+		waitSetValue.Set(reflect.MakeMap(waitSetType))
+	}
+	for key, value := range jsonObject.(map[string]interface{}) {
+		tempValue := reflect.New(waitSetType.Elem())
+		err = objectFillToObjectByReflectWithPositions(value, waitSetType.Elem(), tempValue)
+		if err != nil {
+			return
+		}
+		waitSetValue.SetMapIndex(reflect.ValueOf(key), tempValue)
+	}
+	return
+}
+
+func ObjectFillToBaseType(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
+	switch waitSetType.Kind() {
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		switch reflect.TypeOf(jsonObject).Kind() {
+		case reflect.Uint:
+			waitSetValue.SetUint(uint64(jsonObject.(uint)))
+		case reflect.Uint8:
+			waitSetValue.SetUint(uint64(jsonObject.(uint8)))
+		case reflect.Uint16:
+			waitSetValue.SetUint(uint64(jsonObject.(uint16)))
+		case reflect.Uint32:
+			waitSetValue.SetUint(uint64(jsonObject.(uint32)))
+		case reflect.Uint64:
+			waitSetValue.SetUint(jsonObject.(uint64))
+		case reflect.Uintptr:
+			waitSetValue.SetUint(uint64(jsonObject.(uintptr)))
+		default:
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		switch reflect.TypeOf(jsonObject).Kind() {
+		case reflect.Int:
+			waitSetValue.SetInt(int64(jsonObject.(int)))
+		case reflect.Int8:
+			waitSetValue.SetInt(int64(jsonObject.(int8)))
+		case reflect.Int16:
+			waitSetValue.SetInt(int64(jsonObject.(int16)))
+		case reflect.Int32:
+			waitSetValue.SetInt(int64(jsonObject.(int32)))
+		case reflect.Int64:
+			waitSetValue.SetInt(jsonObject.(int64))
+		case reflect.Float32:
+			waitSetValue.SetInt(int64(jsonObject.(float32)))
+		case reflect.Float64:
+			waitSetValue.SetInt(int64(jsonObject.(float64)))
+		default:
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		}
+	case reflect.Float32, reflect.Float64:
+		switch reflect.TypeOf(jsonObject).Kind() {
+		case reflect.Float32:
+			waitSetValue.SetFloat(float64(jsonObject.(float32)))
+		case reflect.Float64:
+			waitSetValue.SetFloat(jsonObject.(float64))
+		default:
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		}
+	case reflect.String:
+		if reflect.TypeOf(jsonObject).Kind() != reflect.String {
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+			return
+		}
+		waitSetValue.SetString(jsonObject.(string))
+	case reflect.Bool:
+		if reflect.TypeOf(jsonObject).Kind() != reflect.Bool {
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+			return
+		}
+		waitSetValue.SetBool(jsonObject.(bool))
+	default:
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+	}
+	return
+}
