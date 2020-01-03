28103550d52a13be4b3745ecc73160a22584a1eb
zengxianglin <429801517@qq.com>
2020-01-03 15:14:35 +0800

    ObjectFillToObject初版  未调试

--- ObjectFillToToObject.go
+++ ObjectFillToToObject.go
@@ -6,13 +6,20 @@ import (
 	"reflect"
 )
 
+//在遍历
+type subType struct {
+	waitSetType  reflect.StructField
+	waitSetValue reflect.Value
+	index        int
+}
+type SliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)
+
 //fromObject 源数据,directionObject目标数据,sliceHandler Slice处理函数(position为Slice对应所在源数据内的位置,fromSlice该数组的源数据，directionSlice该数组的目标数据)
 //递归源数据内所有字段,一般情况下(字段类型不是Slice) 非Zero的字段覆盖掉目标数据内对应位置的字段
 //该值类型为Slice时分几种情况特殊处理,情况分别如下:
 //1.Slice处理函数为nil时:按照一般情况进行处理
 //2.Slice处理函数不为nil时: 调用处理函数 得到两个变量分别为  覆盖结果和生效与否，如果该次处理结果为不生效，则该字段按照一般情况处理，否则将返回结果直接覆盖到目标字段
-func ObjectFillToObject(fromObject interface{}, directionObject interface{}, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
-
+func ObjectFillToObject(fromObject interface{}, directionObject interface{}, sliceHandler SliceHandler) (err error) {
 	directionType := reflect.TypeOf(directionObject)
 	directionValue := reflect.ValueOf(directionObject)
 	fromType := reflect.TypeOf(fromObject)
@@ -20,11 +27,11 @@ func ObjectFillToObject(fromObject interface{}, directionObject interface{}, sli
 	return ObjectFillToObjectByReflect(fromType, fromValue, directionType, directionValue, sliceHandler)
 }
 
-func ObjectFillToObjectByReflect(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
-	return objectFillToObjectByReflectWithPositions(fromType, fromValue, waitSetType, waitSetValue, []string{}, sliceHandler)
+func ObjectFillToObjectByReflect(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, sliceHandler SliceHandler) (err error) {
+	return objectFillToObjectByReflect(fromType, fromValue, waitSetType, waitSetValue, []string{}, sliceHandler)
 }
 
-func objectFillToObjectByReflectWithPositions(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, positions []string, sliceHandler func(position []string, fromSlice interface{}, directionSlice interface{}) (interface{}, bool)) (err error) {
+func objectFillToObjectByReflect(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value, prePositions []string, sliceHandler SliceHandler) (err error) {
 	if fromValue.IsZero() {
 		//遇到空值的情况直接不进行映射
 		return
@@ -50,9 +57,9 @@ func objectFillToObjectByReflectWithPositions(fromType reflect.Type, fromValue r
 			waitSetValue.Set(reflect.New(waitSetType))
 		}
 		waitSetValue = waitSetValue.Elem()
-		err = objectFillToObjectByReflectWithPositions(fromType, fromValue, waitSetType, waitSetValue, append(positions, waitSetType.Name()), sliceHandler)
+		err = objectFillToObjectByReflect(fromType, fromValue, waitSetType, waitSetValue, prePositions, sliceHandler)
 	case reflect.Struct:
-		err = ObjectFillToStruct(fromType, fromValue, waitSetType, waitSetValue)
+		err = ObjectFillToStruct(fromType, fromValue, waitSetType, waitSetValue, prePositions, sliceHandler)
 	case reflect.Slice:
 		//数组直接进行覆盖
 		waitSetValue.Set(fromValue)
@@ -60,134 +67,202 @@ func objectFillToObjectByReflectWithPositions(fromType reflect.Type, fromValue r
 		//interface直接进行覆盖
 		waitSetValue.Set(fromValue)
 	case reflect.Map:
-		err = ObjectFillToMap(fromType, fromValue, waitSetType, waitSetValue)
+		err = ObjectFillToMap(fromType, fromValue, waitSetType, waitSetValue, prePositions, sliceHandler)
 	default:
 		err = ObjectFillToBaseType(fromType, fromValue, waitSetType, waitSetValue)
 	}
 	return
 }
 
-func ObjectFillToStruct(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
-	if waitSetType.Kind() != reflect.Struct || (fromType.Kind() != reflect.Map && fromType.Kind() != reflect.Struct) {
-		err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, waitSetType))
+func ObjectFillToStruct(fromType reflect.Type, fromValue reflect.Value, directionType reflect.Type, directionValue reflect.Value, prePositions []string, sliceHandler SliceHandler) (err error) {
+
+	//类型不匹配不进行映射  直接返回错误
+	if fromType.Kind() != reflect.Map && fromType.Kind() != reflect.Struct {
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 		return
 	}
-	waitSetMap := map[string]struct {
-		waitSetType  reflect.Type
-		waitSetValue reflect.Value
-	}{}
-	for i := 0; i < waitSetType.NumField(); i++ {
 
+	//将目标转换为Map  方便判断是否存在Key
+	directionSubTypeMap := map[string]subType{}
+	for i := 0; i < directionType.NumField(); i++ {
+		directionSubTypeMap[directionType.Field(i).Name] = subType{directionType.Field(i), directionValue.Field(i), i}
 	}
-	for i := 0; i < waitSetType.NumField(); i++ {
-		valueField := waitSetValue.Field(i)
-		typeField := waitSetType.Field(i)
-		if typeField.Anonymous {
-			err = objectFillToObjectByReflectWithPositions(jsonObject, typeField.Type, valueField)
+	//遍历数据源对象,一一将数据映射存入到目标对象内
+	switch fromType.Kind() {
+	case reflect.Map:
+		for _, key := range fromValue.MapKeys() {
+			keyName, isString := key.Interface().(string)
+			if !isString {
+				continue
+			}
+			subDirection, directionHaveKey := directionSubTypeMap[keyName]
+			if !directionHaveKey {
+				continue
+			}
+			err = objectFillToObjectByReflect(
+				fromValue.MapIndex(key).Type(), fromValue.MapIndex(key).Elem(),
+				subDirection.waitSetType.Type, subDirection.waitSetValue,
+				append(prePositions, keyName), sliceHandler,
+			)
 			if err != nil {
 				return
 			}
-			continue
 		}
-		name := typeField.Name
-		jsonName := typeField.Tag.Get("json")
-		if jsonName == "" {
-			jsonName = name
-		}
-		subField, have := jsonObject.(map[string]interface{})[jsonName]
-		if !have {
-			continue
-		}
-		err = objectFillToObjectByReflectWithPositions(subField, typeField.Type, valueField)
-		if err != nil {
-			return
+	case reflect.Struct:
+		for i := 0; i < fromType.NumField(); i++ {
+			fromValueField := fromValue.Field(i)
+			fromTypeField := fromType.Field(i)
+			if fromTypeField.Anonymous {
+				err = ObjectFillToStruct(fromTypeField.Type, fromValueField, directionType, directionValue, prePositions, sliceHandler)
+				if err != nil {
+					return
+				}
+				continue
+			}
+			keyName := fromTypeField.Name
+			subDirection, directionHaveKey := directionSubTypeMap[keyName]
+			if !directionHaveKey {
+				continue
+			}
+			err = objectFillToObjectByReflect(
+				fromTypeField.Type, fromValueField,
+				subDirection.waitSetType.Type, subDirection.waitSetValue,
+				append(prePositions, keyName), sliceHandler,
+			)
+			if err != nil {
+				return
+			}
 		}
 	}
 	return
 }
 
-func ObjectFillToMap(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
-	jsonObjectType := reflect.ValueOf(jsonObject).Kind()
-	// 假如说双方类型未匹配上，则直接出错退出
-	if jsonObjectType != reflect.Map || waitSetType.Kind() != reflect.Map {
-		err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", jsonObjectType, waitSetType))
+func ObjectFillToMap(fromType reflect.Type, fromValue reflect.Value, directionType reflect.Type, directionValue reflect.Value, prePositions []string, sliceHandler SliceHandler) (err error) {
+	//类型不匹配不进行映射  直接返回错误
+	if fromType.Kind() != reflect.Map && fromType.Kind() != reflect.Struct {
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 		return
 	}
-	if waitSetValue.IsNil() {
-		waitSetValue.Set(reflect.MakeMap(waitSetType))
+	if directionValue.IsNil() {
+		directionValue.Set(reflect.MakeMap(directionType))
 	}
-	for key, value := range jsonObject.(map[string]interface{}) {
-		tempValue := reflect.New(waitSetType.Elem())
-		err = objectFillToObjectByReflectWithPositions(value, waitSetType.Elem(), tempValue)
+	switch directionType.Elem().Kind() {
+	case reflect.Interface:
+		var tempMap interface{}
+		tempMap, err = objectToJsonObjectByReflect(fromType, fromValue)
 		if err != nil {
 			return
 		}
-		waitSetValue.SetMapIndex(reflect.ValueOf(key), tempValue)
+		directionValue.Set(reflect.ValueOf(tempMap))
+	default:
+		subDirectionType := directionType.Elem()
+		switch fromType.Kind() {
+		case reflect.Map:
+			for _, key := range fromValue.MapKeys() {
+				keyName, isString := key.Interface().(string)
+				if !isString {
+					continue
+				}
+				subDirectionValue := reflect.New(subDirectionType)
+				err = objectFillToObjectByReflect(
+					fromValue.MapIndex(key).Type(), fromValue.MapIndex(key).Elem(),
+					subDirectionType, subDirectionValue,
+					append(prePositions, keyName), sliceHandler,
+				)
+				if err != nil {
+					return
+				}
+				directionValue.SetMapIndex(key, subDirectionValue)
+			}
+		case reflect.Struct:
+			for i := 0; i < fromType.NumField(); i++ {
+				fromValueField := fromValue.Field(i)
+				fromTypeField := fromType.Field(i)
+				if fromTypeField.Anonymous {
+					err = ObjectFillToStruct(fromTypeField.Type, fromValueField, directionType, directionValue, prePositions, sliceHandler)
+					if err != nil {
+						return
+					}
+					continue
+				}
+				keyName := fromTypeField.Name
+				subDirectionValue := reflect.New(subDirectionType)
+				err = objectFillToObjectByReflect(
+					fromTypeField.Type, fromValueField,
+					subDirectionType, subDirectionValue,
+					append(prePositions, keyName), sliceHandler,
+				)
+				if err != nil {
+					return
+				}
+				directionValue.SetMapIndex(reflect.ValueOf(keyName), subDirectionValue)
+			}
+		}
 	}
 	return
 }
 
-func ObjectFillToBaseType(fromType reflect.Type, fromValue reflect.Value, waitSetType reflect.Type, waitSetValue reflect.Value) (err error) {
-	switch waitSetType.Kind() {
+func ObjectFillToBaseType(fromType reflect.Type, fromValue reflect.Value, directionType reflect.Type, directionValue reflect.Value) (err error) {
+	switch directionType.Kind() {
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		switch reflect.TypeOf(jsonObject).Kind() {
+		switch fromType.Kind() {
 		case reflect.Uint:
-			waitSetValue.SetUint(uint64(jsonObject.(uint)))
+			directionValue.SetUint(uint64(fromValue.Interface().(uint)))
 		case reflect.Uint8:
-			waitSetValue.SetUint(uint64(jsonObject.(uint8)))
+			directionValue.SetUint(uint64(fromValue.Interface().(uint8)))
 		case reflect.Uint16:
-			waitSetValue.SetUint(uint64(jsonObject.(uint16)))
+			directionValue.SetUint(uint64(fromValue.Interface().(uint16)))
 		case reflect.Uint32:
-			waitSetValue.SetUint(uint64(jsonObject.(uint32)))
+			directionValue.SetUint(uint64(fromValue.Interface().(uint32)))
 		case reflect.Uint64:
-			waitSetValue.SetUint(jsonObject.(uint64))
+			directionValue.SetUint(fromValue.Interface().(uint64))
 		case reflect.Uintptr:
-			waitSetValue.SetUint(uint64(jsonObject.(uintptr)))
+			directionValue.SetUint(uint64(fromValue.Interface().(uintptr)))
 		default:
-			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		switch reflect.TypeOf(jsonObject).Kind() {
+		switch fromType.Kind() {
 		case reflect.Int:
-			waitSetValue.SetInt(int64(jsonObject.(int)))
+			directionValue.SetInt(int64(fromValue.Interface().(int)))
 		case reflect.Int8:
-			waitSetValue.SetInt(int64(jsonObject.(int8)))
+			directionValue.SetInt(int64(fromValue.Interface().(int8)))
 		case reflect.Int16:
-			waitSetValue.SetInt(int64(jsonObject.(int16)))
+			directionValue.SetInt(int64(fromValue.Interface().(int16)))
 		case reflect.Int32:
-			waitSetValue.SetInt(int64(jsonObject.(int32)))
+			directionValue.SetInt(int64(fromValue.Interface().(int32)))
 		case reflect.Int64:
-			waitSetValue.SetInt(jsonObject.(int64))
+			directionValue.SetInt(fromValue.Interface().(int64))
 		case reflect.Float32:
-			waitSetValue.SetInt(int64(jsonObject.(float32)))
+			directionValue.SetInt(int64(fromValue.Interface().(float32)))
 		case reflect.Float64:
-			waitSetValue.SetInt(int64(jsonObject.(float64)))
+			directionValue.SetInt(int64(fromValue.Interface().(float64)))
 		default:
-			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 		}
 	case reflect.Float32, reflect.Float64:
-		switch reflect.TypeOf(jsonObject).Kind() {
+		switch fromType.Kind() {
 		case reflect.Float32:
-			waitSetValue.SetFloat(float64(jsonObject.(float32)))
+			directionValue.SetFloat(float64(fromValue.Interface().(float32)))
 		case reflect.Float64:
-			waitSetValue.SetFloat(jsonObject.(float64))
+			directionValue.SetFloat(fromValue.Interface().(float64))
 		default:
-			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 		}
 	case reflect.String:
-		if reflect.TypeOf(jsonObject).Kind() != reflect.String {
-			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		if fromType.Kind() != reflect.String {
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 			return
 		}
-		waitSetValue.SetString(jsonObject.(string))
+		directionValue.SetString(fromValue.Interface().(string))
 	case reflect.Bool:
-		if reflect.TypeOf(jsonObject).Kind() != reflect.Bool {
-			err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		if fromType.Kind() != reflect.Bool {
+			err = ErrorMaker.GetError(fmt.Sprintf("不能将%v映射到%v中", fromType, directionType))
 			return
 		}
-		waitSetValue.SetBool(jsonObject.(bool))
+		directionValue.SetBool(fromValue.Interface().(bool))
 	default:
-		err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", reflect.TypeOf(jsonObject), waitSetType))
+		err = ErrorMaker.GetError(fmt.Sprintf("不能将%s映射到%v中", fromType, directionType))
 	}
 	return
 }
