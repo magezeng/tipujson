f343c972fd9b0598a7bbb4edee2f1c2b75a62fe6
moke996 <694628923@qq.com>
2020-01-09 10:09:22 +0800

    实现ObjectFillToToObject_test多种类型测试

--- ObjectFillToToObject_test.go
+++ ObjectFillToToObject_test.go
@@ -43,12 +43,14 @@ func TestObjectFillToObject_MapToMap(t *testing.T) {
 	}
 
 	lessons := grade["lesson"].([]interface{})
-
-	if lessons == nil {
-		t.Error("Map对象的lesson字段填充为空")
-	}
 	if len(lessons) != 2 {
 		t.Error("Map对象的lesson字段长度填充错误")
+	}
+	if lessons[0] == nil {
+		t.Error("lesson数组第一项为空")
+	}
+	if lessons[1] == nil {
+		t.Error("lesson数组第二项为空")
 	}
 	if (lessons[0] == "语文" && lessons[1] != "英语") || (lessons[0] == "英语" && lessons[1] != "语文") {
 		t.Error("结构体中的数组内容填充错误")
@@ -240,14 +242,15 @@ func TestObjectFillToObject_StructToMap(t *testing.T) {
 	if grade["name"] != "没希望的一年级" {
 		t.Error("Map对象的name字段填充错误")
 	}
-
 	lessons := grade["lesson"].([]interface{})
-
-	if lessons == nil {
-		t.Error("Map对象的lesson字段填充为空")
-	}
 	if len(lessons) != 2 {
 		t.Error("Map对象的lesson字段长度填充错误")
+	}
+	if lessons[0] == nil {
+		t.Error("lesson数组第一项为空")
+	}
+	if lessons[1] == nil {
+		t.Error("lesson数组第二项为空")
 	}
 	if (lessons[0] == "语文" && lessons[1] != "英语") || (lessons[0] == "英语" && lessons[1] != "语文") {
 		t.Error("结构体中的数组内容填充错误")
@@ -386,7 +389,7 @@ func TestObjectFillToObject_SliceToPtr(t *testing.T) {
 			},
 		},
 	}
-	var grade *Grade
+	var grade *[]Grade
 	err := objectFillToObject(fromObject, &grade, func(position []string, fromSlice interface{}, directionSlice interface{}) (i interface{}, b bool) {
 		return
 	})
@@ -396,31 +399,35 @@ func TestObjectFillToObject_SliceToPtr(t *testing.T) {
 	if grade == nil {
 		t.Error("被填充指针为空")
 	}
-	if grade.Name != "没希望的一年级" {
+	currentGrade := *grade
+	if currentGrade == nil {
+		t.Error("被填充数组为空")
+	}
+	if currentGrade[0].Name != "没希望的一年级" {
 		t.Error("数组中Name项填充错误")
 	}
-	if grade.Lesson == nil {
+	if currentGrade[0].Lesson == nil {
 		t.Error("数组中Lesson项填充为空")
 	}
-	if len(grade.Lesson) != 2 {
+	if len(currentGrade[0].Lesson) != 2 {
 		t.Error("数组中Lesson项长度填充错误")
 	}
-	if (grade.Lesson[0] == "语文" && grade.Lesson[1] != "英语") || (grade.Lesson[0] == "英语" && grade.Lesson[1] != "语文") {
+	if (currentGrade[0].Lesson[0] == "语文" && currentGrade[0].Lesson[1] != "英语") || (currentGrade[0].Lesson[0] == "英语" && currentGrade[0].Lesson[1] != "语文") {
 		t.Error("数组中Lesson项内容填充错误")
 	}
-	if grade.Classes == nil {
+	if currentGrade[0].Classes == nil {
 		t.Error("数组中Classes项填充为空")
 	}
-	if grade.Classes.Age != 15.5 {
+	if currentGrade[0].Classes.Age != 15.5 {
 		t.Error("Map对象的classes字段age项填充错误")
 	}
-	if grade.Classes.Boy != false {
+	if currentGrade[0].Classes.Boy != false {
 		t.Error("Map对象的classes字段age项填充错误")
 	}
-	if grade.Classes.Name != "小红" {
+	if currentGrade[0].Classes.Name != "小红" {
 		t.Error("Map对象的classes字段age项填充错误")
 	}
-	if grade.Classes.Number != 156 {
+	if currentGrade[0].Classes.Number != 156 {
 		t.Error("Map对象的classes字段age项填充错误")
 	}
 }
